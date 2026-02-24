897c83f2982f37b0434d86810a41b630db861505
laraibfarooq12 <laraibfarooq1094@gmail.com>
2026-02-24 22:29:49 +0500

    Added Task 3 - Email Extraction Script

---
+++ CodeAlpha_AutomationScript.py
@@ -0,0 +1,19 @@
+import re
+
+input_file = "input.txt"
+output_file = "emails.txt"
+
+with open(input_file, "r") as file:
+    content = file.read()
+
+emails = re.findall(r"[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]+", content)
+
+with open(output_file, "w") as file:
+    for email in emails:
+        file.write(email + "\n")
+
+print("Extracted Emails:")
+for email in emails:
+    print(email)
+
+print("All emails saved in emails.txt")
